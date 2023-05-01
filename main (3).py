a179cc7a3a72f98421b2d088e360e30bb49ffe00
KusaLLL <114250294+KusaLLL@users.noreply.github.com>
2023-05-01 11:31:12 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,30 @@
+print("This program will calculate the circumference of a circle")
+print("**********")
+
+def circle(radius):
+    "This program will calculate the circumference of a circle"
+    circumference = 2 * 3.14 * r
+    print("The circumference is:", circumference)
+    return
+
+r = float(input("Enter radius: "))
+circle(r)
+
+print()
+print("Now let's calculate the area")
+print()
+
+print("This program will calculate the area of a circle")
+print("**********")
+
+def circle2(radius2):
+    "This program calculates the area of a circle"
+    area = 3.14 * r2 * r2
+    print("The area is:", area)
+    print()
+    print("Thank you!!")
+    return
+
+r2 = float(input("Enter radius: "))
+circle2(r2)
+
